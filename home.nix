076d17532ba77cccc92c941326a2ffe906158cf0
Samuel Shan <samuel.shan@gmail.com>
2025-08-12 20:12:43 +0800

    bash: support history search by fzf

--- home.nix
+++ home.nix
@@ -1,17 +1,11 @@
-{ config, pkgs, ... }:
-
-{
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+{ config, pkgs, system, ... }: let
+  home = if builtins.match ".*-darwin" builtins.currentSystem != null then "/Users/samuel" else "/home/samuel";
+in {
   home.username = "samuel";
-  home.homeDirectory = "/Users/samuel";
+  home.homeDirectory = home;
 
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
-    tmux fasd fzf pstree
-    minicom qemu
+    fzf pstree
 
     # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
@@ -20,29 +14,15 @@
     # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+    ".bash_profile".text = ''
+      export HISTSIZE=5000 HISTCONTROL=ignoreboth:erasedups HISTFILE=~/.bash_history PROMPT_COMMAND="history -a; history -n; $PROMPT_COMMAND"
+      shopt -s histappend
 
-    ".vimrc".text = ''
-        set ai ts=4 sw=4 et
-        set is
-        syntax on
-    '';
-    ".tmux.conf".text = ''
-        unbind C-b
-        set -g prefix `
-        bind ` send-prefix
-        #set-window-option -g mode-keys vi
-        set -w -g mode-keys vi
-        set -g status off
-        set -g mouse on
-        #set -g default-shell /opt/homebrew/bin/fish
-        new-session
+      export EDITOR=vi
+      set -o vi
+
+      source <(fzf --bash)
     '';
   };
 
@@ -63,11 +43,37 @@
   #  /etc/profiles/per-user/samuel/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    EDITOR = "vi";
+    #EDITOR = "vi";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  programs.tmux = {
+    enable = true;
+    extraConfig = ''
+      unbind C-b
+      set -g prefix `
+      bind ` send-prefix
+      #set-window-option -g mode-keys vi
+      set -w -g mode-keys vi
+      set -g status off
+      set -g mouse on
+      #set -g default-shell /opt/homebrew/bin/fish
+      new-session
+    '';
+  };
+  programs.git = {
+    enable = true;
+    userName = "Samuel Shan";
+    userEmail = "samuel.shan@gmail.com";
+  };
+  programs.vim = {
+    enable = true;
+    extraConfig = ''
+      set ai ts=2 sw=2 et
+      set is
+      syntax on
+    '';
+  };
 
-  home.stateVersion = "24.05";
+  home.stateVersion = "25.05";
 }
